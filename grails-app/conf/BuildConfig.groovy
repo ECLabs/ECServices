9d612b9294a1d38ea440710ad57f312de3997aec
ectech <jamil@evanschambers.com>
2012-02-19 00:25:47 -0500

    war file will now be build with the following filename: 'ec-services.war'

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.war.file = "ec-services.war"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
 	pom true
